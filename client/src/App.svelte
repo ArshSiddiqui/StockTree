bdddcb5c14e5761b67fa11104a2766bb6e503c87
arshsiddiqui <arshsiddiqui@gmail.com>
2023-10-23 16:16:26 -0400

    fix login procedures with seperate components

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,30 +1,5 @@
 <script>
-  let text = "Not Connected"
-  let allStocks = ""
-
-  function connect() {
-    fetch("./connect")
-      .then(d => d.text())
-      .then(d => (text = d));
-  }
-
-  function getAllStocks() {
-    fetch("./fetch")
-      .then(d => d.text())
-      .then(d => allStocks = d)
-  }
+  import login from './components/login.svelte';
 </script>
 
-<div id="login-center">
-  <form id="login-box">
-    <label for="username">Username: </label>
-    <input type="text" id="username" name="username"><br>
-    <label for="password">Password: </label>
-    <input type="password" id="pwd" name="pwd">
-  </form>
-  <button on:click={connect}>Connect</button>
-  <h3>{text}</h3>
-  <button on:click={getAllStocks}>GetAllStocks</button>
-  <p>{allStocks}</p>
-</div>
-
+<svelte:component this={login}></svelte:component>
---
+++ client/src/components/login.svelte
@@ -0,0 +1,23 @@
+<script>
+    import StockholderView from "./stockholder_view.svelte";
+
+    let logged_in = false;
+
+    function login() {
+       logged_in = true;
+    }
+</script>
+
+{#if logged_in == false}
+    <div id="login-center">
+      <form id="login-box">
+        <label for="username">Username: </label>
+        <input type="text" id="username" name="username"><br>
+        <label for="password">Password: </label>
+        <input type="password" id="pwd" name="pwd">
+      </form>
+      <button on:click={login}>Login</button>
+    </div>
+{:else}
+    <svelte:component this={StockholderView}></svelte:component>
+{/if}
